3748293b6dbad999e9d22e0d9d33d3ca8039417b
Tasnimul-Shara <tasnimulsharan8962@gmail.com>
2024-08-18 11:43:13 +0600

    updated

--- backend/Index.js
+++ backend/Index.js
@@ -1,12 +1,43 @@
 // src/index.js
-require('dotenv').config();
-const itemRoutes = require('./routes/itemRoutes');
-const errorHandler = require('./middleware/errorHandler');
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const errorHandler = require("./middleware/errorHandler");
+const connectDB = require("./config/db");
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 5000;
+const productRoutes = require("./routes/productRoutes");
 
+// const { MongoClient, ServerApiVersion } = require("mongodb");
+// const uri =
+//   "mongodb+srv://tasnimulalam303:OfElGhmd6qhyMkiC@cluster0.eyzut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log(
+//       "Pinged your deployment. You successfully connected to MongoDB!"
+//     );
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
@@ -14,7 +45,7 @@ app.use(express.json());
 
 // Define routes
 // const itemRoutes = require('./routes/items');
-app.use('/api/items', itemRoutes);
+app.use("/api", productRoutes);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
--- backend/config/db.js
+++ backend/config/db.js
@@ -17,32 +17,46 @@
 
 // module.exports = db;
 
-require('dotenv').config();
-const mysql = require('mysql2');
-const db = mysql.createPool({
-  connectionLimit: 10,
-  host: `${process.env.DB_HOST}`,
-  user: `${process.env.DB_USERNAME}`,
-  password: `${process.env.DB_PASSWORD}`,
-  database: `${process.env.DB_DATABASE}`,
-});
+// require('dotenv').config();
+// const mysql = require('mysql2');
+// const db = mysql.createPool({
+//   connectionLimit: 10,
+//   host: `${process.env.DB_HOST}`,
+//   user: `${process.env.DB_USERNAME}`,
+//   password: `${process.env.DB_PASSWORD}`,
+//   database: `${process.env.DB_DATABASE}`,
+// });
 
-db.getConnection((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err);
-    return;
+// db.getConnection((err) => {
+//   if (err) {
+//     console.error('Error connecting to MySQL:', err);
+//     return;
+//   }
+//   console.log('Connected to MySQL');
+// });
+
+// db.on('error', (err) => {
+//   if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+//     console.log('MySQL connection was lost. Reconnecting...');
+//     db.connect();
+//   } else {
+//     throw err;
+//   }
+// });
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      //   useNewUrlParser: true,
+      //   useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
   }
-  console.log('Connected to MySQL');
-});
+};
 
-db.on('error', (err) => {
-  if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-    console.log('MySQL connection was lost. Reconnecting...');
-    db.connect();
-  } else {
-    throw err;
-  }
-});
-
-
-module.exports = db;
+module.exports = connectDB;
